1c1bf92efdb9cec3314a2731396b216414c4389c
chavwithtache <32267135+chavwithtache@users.noreply.github.com>
2017-09-26 10:31:51 +0100

    refactor

--- src/GetValues.py
+++ src/GetValues.py
@@ -10,17 +10,20 @@ def write_dictionary_as_json_file(filename, dictionary):
     with open('../data/' + filename, 'w') as f:
         f.write(json.dumps(dictionary, indent=4))
 
+def add_result(crypto_data, coin, balance, price):
+    crypto_data['balances'][coin] = balance
+    crypto_data['prices'][coin] = price
+    crypto_data['values'][coin] = price * balance
+    return crypto_data
+
 
 crypto_data = {'balances': {}, 'prices': {}, 'values': {}}
-crypto_prices = crypto_data['prices']
-crypto_values = crypto_data['values']
+
 
 # config
 config = json.loads(open('../config/config.json').read())
 crypto_balances = json.loads(open('../data/crypto_balances.json').read())['coins']
 simple_balances = json.loads(open('../data/crypto_balances_aggregated.json').read())['coins']
-
-
 
 
 cmc_config = config['api_config']['coinmarketcap']
@@ -32,10 +35,7 @@ display_ccy = cmc_config['display_ccy']
 for coin, balance in simple_balances['crypto'].items():
     cmc_id = coin_config[coin]['coinmarketcap_id']
     url = cmc_config['url'] + cmc_id + '?convert=' + display_ccy
-    crypto_prices[coin] = float(json.loads(urllib.request.urlopen(url).read())[0]['price_' + display_ccy.lower()])
-    crypto_values[coin] = crypto_prices[coin] * balance
-    #also write the balance into the output file
-    crypto_data['balances'][coin] = balance
+    crypto_data = add_result(crypto_data, coin, balance, float(json.loads(urllib.request.urlopen(url).read())[0]['price_' + display_ccy.lower()]))
 
 
 # get fiat prices from fixer.io
@@ -45,13 +45,10 @@ fxrates = json.loads(urllib.request.urlopen(url).read())
 
 for coin, balance in simple_balances['fiat'].items():
     if coin == base_ccy:
-        crypto_prices[coin] = 1
-        crypto_values[coin] = balance
+        crypto_data = add_result(crypto_data, coin, balance, 1)
     else:
-        crypto_prices[coin] = 1 / fxrates['rates'][coin]
-        crypto_values[coin] = crypto_prices[coin] * balance
-    # also write the balance into the output file
-    crypto_data['balances'][coin] = balance
+        crypto_data = add_result(crypto_data, coin, balance, 1 / fxrates['rates'][coin])
+
 
 
 #Try BLX using some dodgy hack
@@ -59,11 +56,10 @@ iconomi_config = config['api_config']['iconomi_blx']
 usdrate = fxrates['rates']['USD']
 for coin, balance in simple_balances['iconomi_fund'].items():
     price_usd = float(json.loads(urllib.request.urlopen(iconomi_config['url']+coin+'-chart').read())['chartData'].pop()['y']['tokenPrice'])
-    crypto_prices[coin] = price_usd / usdrate
-    crypto_values[coin] = crypto_prices[coin] * balance
-    crypto_data['balances'][coin] = balance
+    crypto_data = add_result(crypto_data, coin, balance, price_usd / usdrate)
 
-values = [crypto_values[x] for x in crypto_values]
+
+values = [crypto_data['values'][x] for x in crypto_data['values']]
 total = '{:0,.2f}'.format(sum(values))
 print('£' + total)
 crypto_data['total_value'] = total
--- src/GetValues_ByCoin.py
+++ src/GetValues_ByCoin.py
@@ -10,16 +10,22 @@ def write_dictionary_as_json_file(filename, dictionary):
     with open('../data/' + filename, 'w') as f:
         f.write(json.dumps(dictionary, indent=4, sort_keys=True))
 
+def add_result(crypto_data, coin, balance, price):
+    crypto_data_coins = crypto_data['coins']
+    crypto_data_coins[coin] = {}
+    crypto_data_coins[coin]['balance'] = balance
+    crypto_data_coins[coin]['price'] = price
+    crypto_data_coins[coin]['value'] = price * balance
+    return crypto_data
+
 
 crypto_data = {'coins': {}}
-crypto_data_coins = crypto_data['coins']
+
 
 # config
 config = json.loads(open('../config/config.json').read())
 crypto_balances = json.loads(open('../data/crypto_balances.json').read())['coins']
 simple_balances = json.loads(open('../data/crypto_balances_aggregated.json').read())['coins']
-
-
 
 
 cmc_config = config['api_config']['coinmarketcap']
@@ -31,11 +37,7 @@ display_ccy = cmc_config['display_ccy']
 for coin, balance in simple_balances['crypto'].items():
     cmc_id = coin_config[coin]['coinmarketcap_id']
     url = cmc_config['url'] + cmc_id + '?convert=' + display_ccy
-    crypto_data_coins[coin]={}
-    crypto_data_coins[coin]['price'] = float(json.loads(urllib.request.urlopen(url).read())[0]['price_' + display_ccy.lower()])
-    crypto_data_coins[coin]['value'] = crypto_data_coins[coin]['price'] * balance
-    #also write the balance into the output file
-    crypto_data_coins[coin]['balance'] = balance
+    crypto_data = add_result(crypto_data, coin, balance, float(json.loads(urllib.request.urlopen(url).read())[0]['price_' + display_ccy.lower()]))
 
 
 # get fiat prices from fixer.io
@@ -44,15 +46,11 @@ url = fixer_config['url'] + '?symbols=' + ','.join([coin for coin in simple_bala
 fxrates = json.loads(urllib.request.urlopen(url).read())
 
 for coin, balance in simple_balances['fiat'].items():
-    crypto_data_coins[coin] = {}
     if coin == base_ccy:
-        crypto_data_coins[coin]['price'] = 1
-        crypto_data_coins[coin]['value'] = balance
+        crypto_data = add_result(crypto_data, coin, balance, 1)
     else:
-        crypto_data_coins[coin]['price'] = 1 / fxrates['rates'][coin]
-        crypto_data_coins[coin]['value'] = crypto_data_coins[coin]['price'] * balance
-    # also write the balance into the output file
-    crypto_data_coins[coin]['balance'] = balance
+        crypto_data = add_result(crypto_data, coin, balance, 1 / fxrates['rates'][coin])
+
 
 
 #Try BLX using some dodgy hack
@@ -60,12 +58,10 @@ iconomi_config = config['api_config']['iconomi_blx']
 usdrate = fxrates['rates']['USD']
 for coin, balance in simple_balances['iconomi_fund'].items():
     price_usd = float(json.loads(urllib.request.urlopen(iconomi_config['url']+coin+'-chart').read())['chartData'].pop()['y']['tokenPrice'])
-    crypto_data_coins[coin] = {}
-    crypto_data_coins[coin]['price'] = price_usd / usdrate
-    crypto_data_coins[coin]['value'] = crypto_data_coins[coin]['price'] * balance
-    crypto_data_coins[coin]['balance'] = balance
+    crypto_data = add_result(crypto_data, coin, balance, price_usd / usdrate)
 
-values = [x['value'] for x in crypto_data_coins.values()]
+
+values = [x['value'] for x in crypto_data['coins'].values()]
 total = '{:0,.2f}'.format(sum(values))
 print('£' + total)
 crypto_data['total_value'] = total
